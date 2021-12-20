6ee057901e0c66aa29e7eaa2e4c2beaf15530364
eunyechan <glqgkq384@naver.com>
2021-12-20 15:04:59 +0900

    coin gh

---
+++ src/theme.ts
@@ -0,0 +1,20 @@
+import { DefaultTheme } from "styled-components";
+
+export const darktheme: DefaultTheme = {
+  bgColor: "#2c3e50",
+  textColor: "#95a5a6",
+  accentColor: "#bdc3c7",
+};
+
+export const lighttheme: DefaultTheme = {
+  bgColor: "white",
+  textColor: "black",
+  accentColor: "#8c7ae6",
+};
+
+export const theme = {
+  lighttheme,
+  darktheme,
+};
+
+export default theme;
